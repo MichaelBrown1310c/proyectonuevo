82bfa8a54ac38895c06628d2d4a8289c1fb710e9
Estudiante USC <estudiante@GTACAD.USC>
2023-02-18 16:39:11 -0500

    nuevo commit

--- lista de estudiantes/src/Principal.java
+++ lista de estudiantes/src/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 public static void main(String[] args) {
 	
 	Student std = new Student();
-	System.out.println(std.iD + "\n" + std.name + "\n" + std.status);
+	System.out.println(std.iD + "\n" + std.name + "\n" + std.status); 
 
 }
 }
